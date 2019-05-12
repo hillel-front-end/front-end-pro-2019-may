d4d9c601e7ba30cd3d4e22790d220257fc4ada80
Your Name <you@example.com>
2019-05-12 14:26:17 +0300

    l-1

---
+++ lections/lection_1_Intro/materials/lections/main2.js
@@ -0,0 +1,53 @@
+console.log('Hello world');
+
+var a = '243', b = '123', c = 0;
+
+
+// console.log('c', c);
+
+// ----- #6 Базовые операторы( +,-,*,/,% ) -------
+
+// c = a + b;
+// c = a * b;
+// console.log('c', c);
+// c = a / b;
+// console.log('Тут пер-ная с', c);
+// c = a - b;
+// console.log('c', c);
+
+// с = a % b;
+
+console.log('Остаток: ', c);
+
+
+//-----------------------
+var a = '243', b = '123', c = 0;
+
+
+console.log(a, 'тут -  a')
+
+c = a + b;
+console.log('+', c);
+
+
+c = a - b;
+console.log('-', c);
+
+c = a * b;
+console.log('-', c);
+
+c = a / b;
+console.log('-', c);
+
+// --------------
+
+console.log('string', a )
+//  c = +a  + +b; // 213 + +'123'
+
+c = parseInt(a)  + parseInt(b); // 213 + 123
+
+console.log('переменная "C" = ', c);
+
+// b = 'some text';
+// c = +b;
+console.log(typeof c);
