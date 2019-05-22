8cbef149718a0dad271d9d35149eb4d94e9e6a28
Your Name <you@example.com>
2019-05-22 18:34:05 +0300

    L-3.1

--- lections/lection_3_arrays/materials/lections/main.js
+++ lections/lection_3_arrays/materials/lections/main.js
@@ -25,3 +25,78 @@ Lection 3 - Array
     Math.random() * (max - min) + min;
 */
 
+// ----------------------- Math -------------
+
+// console.log(Math.round(1.3), 'Math.round');
+
+// console.log(Math.ceil(1.5), 'math ceil');
+
+// console.log(Math.floor(1.5), 'floor');
+
+// console.log(Math.floor(Math.random()), 'random');
+
+
+// console.log(Math.ceil(Math.random()), 'random');
+
+// console.log(Math.ceil(Math.random() * (30 - 10) + 10));
+
+// ----------------------- Array -------------
+
+// var array = ["user1", "user2", "user3"];
+
+// var a = 1, b  = 2, c = 3;
+// console.log(array, 'array');
+
+// var mass = [a, b, c];
+
+// console.log(mass, 'mass');
+
+// console.log(mass[1], 'mass[1]');
+// console.log(mass[0], 'mass[0] -- last');
+// console.log(mass.length, 'length');
+
+// mass[mass.length] = 'foo';
+
+// console.log(mass, 'mass');
+
+// mass[mass.length] = 12321;
+
+// console.log(mass.length, 'mass.length');
+
+// console.log(mass[mass.length - 1], 'mass.length - 1');
+
+// ------------------------------- new Array()
+// var mass = new Array(10, 10, 'asdasd', 213);
+
+// console.log(mass, 'mass with new Array(10)');
+
+
+// mass.length = 50;
+// var massSecond = [];
+
+
+// massSecond[101] = '213213';
+
+// console.log(massSecond, 'mass');
+
+//----------------- 
+
+var mass = [];
+
+mass.length = 10;
+
+for (var i = 0; i < mass.length ; i++) {
+  mass[i] = Math.floor(Math.random()* (30 - 10) + 10);
+}
+
+
+
+for (var i = 0;  i < mass.length; i++) {
+  var counter = 0;
+  for (var j = 3; 15 <= 15; j++) {
+    if(  15 % 3 == 0){
+      counter ++;
+    }
+  }
+}
+
