cd6097f99e09ddd6a5459a51cdc0e45647bf45da
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2019-05-29 15:52:58 +0300

    l-5

---
+++ lections/lection_5_functions/materials/lections/main.js
@@ -0,0 +1,12 @@
+/*
+  Lection 5 - Array
+    * example func declaration
+    * example func expression
+    * use/call func
+    * use/call func without args
+    * use/call func with args
+    * arguments
+    * callback
+    * scope
+*/
+
