aaa53e150fcbfaaeae0e87de69ace783a4083802
MrTernavskiy <valerater@ukr.net>
2019-07-14 13:02:28 +0300

    L--15

---
+++ lections/lection_15_OOP_practice/materials/main.js
@@ -0,0 +1,36 @@
+// var obj = { a: 1 }; 
+
+// (function() {  /// var obj = obj
+//   var  obj = LE.scope.obj;
+//   LE = {obj} 
+
+//   LE.obj = {
+//       a:2
+//   }
+
+//     return o
+// })(obj)
+
+// (function f(){
+//     LE = {}
+//     function f(){ return 1; }
+//     LE = {f:func() { return 1}}
+//     function f(){ return 2; }
+//     LE = {f:func() { return 2}}
+//     return f();
+  
+//   })();
+
+
+  // var mass = [1,2,3];
+
+  // function isEvent (value) {
+  //   return  value % 2 == 0
+  // }
+
+  // function filter(arr, func) {
+  //   // console.log(arr,'arr')
+  //   // console.log(if(func(arr[i]), 'func')
+  // }
+
+  // filter([1,2,3], isEvent);
