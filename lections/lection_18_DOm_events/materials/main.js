0a50ed25142b073509a450eb6070ea0fb6062e3f
VTernavskiy <Valeriy.Ternavskiy@alfabank.kiev.ua>
2019-07-24 16:01:43 +0300

    L-18

---
+++ lections/lection_18_DOm_events/materials/main.js
@@ -0,0 +1,17 @@
+/*
+   1. attrs: getAttribute, setAttribute, removeAttribute,
+    dataset,
+   2. createElement
+   3. appendChild
+   4. Навесить событие на дин. элем.
+   5. cycles event
+   ?6. navigation with node
+
+*/
+
+
+
+window.onload = function() {
+
+
+}
