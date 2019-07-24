0df884e4fe5001bca73373097381cf3b0d017d71
MrTernavskiy <valerater@ukr.net>
2019-07-24 18:35:35 +0300

    l-18

--- lections/lection_18_DOm_events/materials/main.js
+++ lections/lection_18_DOm_events/materials/main.js
@@ -12,6 +12,65 @@
 
 
 window.onload = function() {
+    // var div = document.querySelector('.foo');
+
+    // console.dir(div, 'div');
+    // div.setAttribute('cry', '12321');
+        // div.cry = 'aaaaa';
+    //  console.log(div.getAttribute('foo'));
+        // setTimeout(function() {
+        //     div.removeAttribute('cry');
+        // }, 4000)
+
+// ------------------ dataset -----------
 
 
-}
+    // console.log(div.dataset.name, 'name');
+
+
+    // var div = document.createElement('div');
+    
+    // div.classList.add('active');
+    // div.dataset.action = 'getAllStudets';
+
+    // div.innerText = 'SomeText';
+    // var span = document.createElement('span');
+    // span.innerHTML = "text";
+
+    // div.appendChild(span);
+    
+    // document.querySelector('.foo').appendChild(div);
+
+
+    // console.dir(div, 'div');
+
+    var container = document.querySelector('.container');
+
+
+    var modalMessages = [
+        {name:'Valera', messages: 'Bla bla bla ....'},
+        {name:'Valera', messages: 'Bla bla bla ....'},
+        {name:'Valera', messages: 'Bla bla bla ....'},
+        {name:'Valera', messages: 'Bla bla bla ....'},
+    ]
+    var fragment = document.createDocumentFragment(); // псевдо тег, растворится после аппенда в ДОм
+
+    console.dir(fragment, 'fragment');
+    
+    for(var i = 0; i < modalMessages.length; i++) {
+        var message = document.createElement('section');
+        message.innerHTML = modalMessages[i].messages;
+        fragment.appendChild(message);
+
+    }
+
+    container.appendChild(fragment);
+
+
+    setTimeout(function(){
+        // document.body.removeChild(container); 
+        container.remove();
+    }, 5000)
+
+
+} 
