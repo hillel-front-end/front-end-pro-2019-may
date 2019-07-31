13dd258c684129c9e291fba257f15f856d279e42
MrTernavskiy <valerater@ukr.net>
2019-07-31 19:02:59 +0300

    L-20-practice

--- lections/lection_20_Storage-practice/materials/main.js
+++ lections/lection_20_Storage-practice/materials/main.js
@@ -12,28 +12,58 @@ window.onload = function() {
     // localStorage.removeItem('foo')
     // var a = 'foo'; 
     // localStorage[a];
- console.dir();
-var adress = {
-    country: 'Ukrine',
-    city: 'Kiev',
-    list: [1,2,3,4,5],
-    x: 12
-};
+//  console.dir();
+// var adress = {
+//     country: 'Ukrine',
+//     city: 'Kiev',
+//     list: [1,2,3,4,5],
+//     x: 12
+// };
 
 
- localStorage.setItem('foo', adress);
 
-//  document.write()
+//  localStorage.setItem('foo', adress);
 
-var adressToStr = JSON.stringify(adress);
+// //  document.write()
 
-console.log(adressToStr, 'adressToStr');
+// var adressToStr = JSON.stringify(adress);
+// console.log(adressToStr, 'adressToStr');
 // localStorage.setItem('adressToStr', adressToStr);
 
 // var foo =  JSON.parse(localStorage.getItem('adressToStr'));
 
 // console.log(foo, 'foo');
 // localStorage.removeItem('adressToStr');
+
+var blocks = document.querySelectorAll('.block');
+
+
+console.log(blocks, 'blocks');
+
+for(var i = 0; i < blocks.length; i++) {
+    blocks[i].addEventListener('click', function(event) {
+        var target = event.target;
+
+        if (target.classList.contains('button')) {
+            setCounter(i, this);
+        }
+
+    });
+
+
+    function setCounter(i, self) {
+       var counter = self.querySelector('.counter');
+       var count = ++self.querySelector('.counter').innerHTML;
+
+       counter.innerHTML = count;
+    //    localStorage
+
+
+         console.log(i, 'i');
+       console.log(counter, 'counter');
+
+    }
+}
 
 
 }
