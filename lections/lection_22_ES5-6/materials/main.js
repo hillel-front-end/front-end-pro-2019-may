0b415d18f560e7cab16d6907a34eb8b4ce0fd6fd
VTernavskiy <Valeriy.Ternavskiy@alfabank.kiev.ua>
2019-08-21 16:06:12 +0300

    l-22

---
+++ lections/lection_22_ES5-6/materials/main.js
@@ -0,0 +1,92 @@
+// console.log('Lection_1');
+
+
+// /*
+// - let,const
+// - Arrow Functions
+// - Template Literals // tag templates
+
+// - Object Properties
+// - Destructuring
+
+// */
+
+// // var x = 10;
+
+// // function f() {
+    
+
+// //     console.log(x);
+// //     // var x;
+
+// //     return x;
+// // }
+
+// // f();
+
+
+// // let, const
+
+
+
+
+// // ----------------------
+
+
+
+// // ------------------- arrow func -----------------
+
+
+// // let arr = [21, 71, 8, 74, 8, 3, 4, 12];
+
+// // let res = arr.filter(function(item){
+// //     return item < 10;
+// // });
+
+
+// // ------------------- Template Literals ------------------- 
+
+
+let a = 123, b = 'Gggg', c = "Gfff";
+
+
+
+
+// -----------------------------------------
+
+// --------------- defaults params -------------------
+
+// function sum(a, b, data){
+//     // if (a === undefined || b === undefined) {
+//     //     return console.error('parametr not found')
+//     // }
+    // a = a || 0;
+    // b = b || 0;
+
+    // if(!data || !data.x) {
+    //     data = {
+    //         x: 0
+    //     }
+    // }
+    
+//     return a + b;
+// }
+
+
+// // ---------------- object method --------------
+
+// let obj = {
+//     x: 10,
+//     f: function(){},
+// }
+
+// // console.log(obj);
+
+//  -------------------------------------
+
+// -------------------- Destructuring (obj, arr) ---------------------
+
+
+//---------------------------------------------------
+
+
