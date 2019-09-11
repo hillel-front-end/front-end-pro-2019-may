cab5a80406b97eb566448a2c75341c14ec3127dd
MrTernavskiy <valerater@ukr.net>
2019-09-11 18:45:09 +0300

    L-26

---
+++ lections/lection_26_vuejs_components/materials/component.js
@@ -0,0 +1,47 @@
+Vue.component('Nav-Item', {
+    props:{
+        foo: Number, 
+        current: String
+    },
+    data() {
+        return {
+      
+        }
+    },
+    template: `
+    <div class="navigation__item" :class='current'>
+        {{foo}}
+        {{current}}
+    </div>
+`
+});
+
+
+Vue.component('Navigation', {
+    data() {
+        return {
+            a: 123,
+            navigationList: ['Home','News', 'Contacts', 'Gallery']
+        }
+    },
+    template: `
+    <div class="navigation">
+        <Nav-Item v-for="item in navigationList" v-bind:current="a" v-bind:foo="item"> </Nav-Item>
+    </div>
+`
+});
+
+
+
+
+let config = {
+    name: "Valera",
+    list: ["red"]
+  };
+
+var app = new Vue({
+    el:'#app',
+    data: config
+});
+
+
---
+++ lections/lection_26_vuejs_components/materials/main.js
@@ -0,0 +1,32 @@
+// new Vue({
+//   el:'#app',
+//   data: {
+//     name: "Valera",
+//     list: [1,2,3]
+//   },
+//   beforeCreate () {
+//     // console.log('beforeCreate', this);
+//     // debugger;
+//   },
+//   created () {
+//     // console.log('created', this);
+//     // debugger;
+//   },
+//   beforeMount () {
+//     // console.log('beforeMount');
+//     // debugger;
+//   },
+//   mounted () {
+//     // console.log('mounted', this);
+//     // debugger;
+//   },
+//   beforeUpdate () {
+//     console.log('beforeUpdate');
+//     debugger;
+//   },
+//   updated () {
+//     console.log('updated');
+//     debugger;
+//   }
+
+// });
