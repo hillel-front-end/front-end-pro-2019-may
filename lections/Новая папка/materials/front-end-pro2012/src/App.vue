cdbbbab29fe8e8d9fae4040c3e9fdcdca1a56051
MrTernavskiy <valerater@ukr.net>
2019-09-15 14:18:24 +0300

    L-27

---
+++ lections/Новая папка/materials/front-end-pro2012/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    Parent: <input type="text" v-model="goodValue">{{goodValue}}
+    <hr />
+
+    <HelloWorld @transfer="getValue" :propValue="goodValue"/>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      goodValue: ''
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+    getValue(value) {
+      console.log('Working ..getValue', value)
+      this.goodValue = value;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ lections/Новая папка/materials/front-end-pro2012/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    inChild: <input type="text" v-bind:value="propValue" @input="onInput">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props:['propValue'],
+  data() {
+    return {
+    }
+  },
+  methods: {
+    onInput(event) {
+      console.log(event.target.value, 'value in child')
+      this.$emit('transfer', event.target.value);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
